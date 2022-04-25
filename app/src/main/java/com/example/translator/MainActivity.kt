ca789d2698f8d04b8dceaa4204caa0b6a856a676
ts moosavi <tt.musavi@gmail.com>
2022-04-25 17:56:02 +0430

    splash screen added

--- app/src/main/java/com/example/translator/MainActivity.kt
+++ app/src/main/java/com/example/translator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.translator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.translator.databinding.ActivityMainBinding
 
@@ -9,6 +10,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      binding =DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding =DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportActionBar?.hide()
+        binding.splashScreen.alpha = 0f
+        binding.splashScreen.animate().setDuration(6000).alpha(1f).withEndAction{
+            binding.homeFragment.visibility = View.VISIBLE
+            binding.splashScreen.visibility = View.GONE
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            supportActionBar?.show()
+        }
     }
 }
