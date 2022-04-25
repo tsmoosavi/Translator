4176c670ff121b33441aeb3cd7f56960f631db29
ts moosavi <tt.musavi@gmail.com>
2022-04-25 18:13:02 +0430

    word entity added

---
+++ app/src/main/java/com/example/translator/WordEntity.kt
@@ -0,0 +1,14 @@
+package com.example.translator
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class WordEntity (
+    @PrimaryKey(autoGenerate = true) val id:Int,
+    val word: String,
+    val meaningOfWord: String,
+    val example: String,
+    val synonym: String,
+    val url: String
+)
