69a6759cff0e4d1cc4c49ec3e820e08cc1a70c5f
ts moosavi <tt.musavi@gmail.com>
2022-04-26 01:19:08 +0430

    AddWordFragment.kt added

---
+++ app/src/main/java/com/example/translator/AddWordFragment.kt
@@ -0,0 +1,26 @@
+package com.example.translator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.translator.databinding.FragmentAddWordBinding
+
+class AddWordFragment : Fragment() {
+    lateinit var binding: FragmentAddWordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_word,container,false)
+        return binding.root
+    }
+}
