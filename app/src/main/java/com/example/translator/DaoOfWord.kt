78003d31704b47b2e8cae0201e7de3f9692b2fa7
ts moosavi <tt.musavi@gmail.com>
2022-04-25 18:38:54 +0430

    dao added

---
+++ app/src/main/java/com/example/translator/DaoOfWord.kt
@@ -0,0 +1,21 @@
+package com.example.translator
+
+import androidx.room.*
+
+@Dao
+interface DaoOfWord {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addWord(word: WordEntity)
+
+    @Delete
+    fun delete(word: WordEntity)
+
+    @Update
+    fun edit(word: WordEntity)
+
+    @Query ("SELECT * FROM WordEntity WHERE word = :word")
+    fun getWord(word: String): WordEntity
+
+    @Query ("SELECT * FROM WordEntity WHERE meaningOfWord = :word")
+    fun getMeaningOfWord(word: String): WordEntity
+}
