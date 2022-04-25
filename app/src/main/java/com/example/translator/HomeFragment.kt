a7e07c7a2bfc2ebc064ea26f2efee8d1a2e88dd7
ts moosavi <tt.musavi@gmail.com>
2022-04-25 23:21:53 +0430

    a shape added for textviews

--- app/src/main/java/com/example/translator/HomeFragment.kt
+++ app/src/main/java/com/example/translator/HomeFragment.kt
@@ -24,4 +24,7 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
