fbaf6f6ef047a4905ea605dab0d8682c95f08052
ts moosavi <tt.musavi@gmail.com>
2022-04-26 23:14:40 +0430

    showViewModel added

---
+++ app/src/main/java/com/example/translator/viewModel/ShowVm.kt
@@ -0,0 +1,16 @@
+package com.example.translator.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.translator.database.Repository
+import com.example.translator.database.WordEntity
+
+class ShowVm(app: Application): AndroidViewModel(app)  {
+    val repo = Repository()
+    init {
+        repo.startDb(app.applicationContext)
+    }
+    fun findId(id: Int):WordEntity{
+        return repo.getId(id)
+    }
+}
